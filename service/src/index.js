84cc72c114112726f7c6b070b87ca3a75f799abe
Qendresa Hoti <qendresa@butlerhospitality.com>
2021-09-09 09:41:15 +0200

    saving items in the db and serverless functions added

---
+++ service/src/index.js
@@ -0,0 +1,44 @@
+var AWS = require("aws-sdk");
+
+AWS.config.update({
+    region: "us-west-2",
+    accessKeyId: "AKIA6HNCZ5YCHBK343UC",
+    secretAccessKey: "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"
+});
+
+module.exports.getJewelry = async (event, context) => {
+    const docClient = new AWS.DynamoDB.DocumentClient();
+
+    const table = "serverless-present-x";
+
+    const params = {
+        TableName:table,
+        Item:{
+            "pk": "product",
+            "sk": "product::123",
+            "style": "Sapphire",
+            "metal": "silver",
+            "shape": "round"
+        }
+    };
+
+    console.log("Adding a new item...");
+    try {
+       await docClient.put(params).promise();
+    } catch (e) {
+       console.log("error", e);
+       return {
+           code: 500,
+           body: JSON.stringify({
+               message: "error?"
+           })
+       }
+    }
+    return {
+        code: 200,
+        body: JSON.stringify({
+            message: "euro?"
+        })
+    }
+}
+
